2721cac286e41b30f72eb9db0bb901e75a1681a0
isaaccidreira <isaaccidreira@hotmail.com>
2022-07-07 11:46:59 -0300

    feat: adicionado testes

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -18,7 +18,11 @@ class CreateRentalUseCase {
     @inject('DayjsDateProvider')
     private dateProvider: IDateProvider,
   ) {}
-  async execute({ user_id, car_id, expected_return_date }): Promise<Rental> {
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
     const minimumHour = 24;
 
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
