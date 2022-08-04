da1244637f1cbd916eb1ffb5fdcaa659649bc710
isaaccidreira <isaaccidreira@hotmail.com>
2022-08-04 10:00:00 -0300

    product configuration

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  presets: [],
-  plugins: [],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    ['module-resolver'],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
 };
